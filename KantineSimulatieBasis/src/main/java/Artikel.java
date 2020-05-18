eb6e325415a556224dba94c650cc6b00ddf4b05a
Lucofski <58174211+Lucofski@users.noreply.github.com>
2020-05-18 19:35:28 +0200

    Add files via upload
    Added KantineSimulatieBasis

---
+++ KantineSimulatieBasis/src/main/java/Artikel.java
@@ -0,0 +1,17 @@
+public class Artikel {
+
+    String naam;
+    float prijs;
+
+    public Artikel(String naam, float prijs) {
+        naam = NAAM;
+        prijs = 0;
+    }
+
+    //public Artikel() {
+    //    naam = NAAM;
+    //    prijs = 0;
+    //}
+
+    
+}
